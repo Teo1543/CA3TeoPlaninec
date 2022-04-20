2af47eeadb2fe4c29f9c5f7838284f23852f4955
Teo Planinec <20093089@mail.wit.ie>
2022-04-20 20:25:42 +0100

    Main menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,29 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import utils.ScannerInput
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+}
+
+
+fun mainMenu(): Int {
+    return ScannerInput.readNextInt(
+        """ 
+         > --------------------------------------
+         > |       Police Officers APP             |
+         > --------------------------------------
+         > | NOTE MENU                          |
+         > |   1) Add a Police Officers                    |
+         > |   2) List Police Officers                    |
+         > |   3) (custom method)|
+         > |   4) Update a Police Officers                 |
+         > |   5) Delete a Police Officers                 |
+         > |   6) (custom method)               |
+         > |   7) Search Police Officers                  |
+         > --------------------------------------
+         > |   20) Save Police Officers                   |
+         > |   21) Load Police Officers                   |
+         > |   0) Exit                          |
+         > --------------------------------------
+         > ==>> """.trimMargin(">")
+    )
 }
