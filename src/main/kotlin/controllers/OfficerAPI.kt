9dcf87f47d2a50e558671bca6fff012e0f6f8ef0
Teo Planinec <20093089@mail.wit.ie>
2022-04-20 19:37:02 +0100

    add method

--- src/main/kotlin/controllers/OfficerAPI.kt
+++ src/main/kotlin/controllers/OfficerAPI.kt
@@ -1,4 +1,16 @@
 package controllers
 
+import models.Officer
+
 class OfficerAPI {
+
+    private var officers = ArrayList<Officer>();
+
+    fun add(officer: Officer): Boolean {
+        return officers.add(officer);
+    }
+
+
+
+
 }
