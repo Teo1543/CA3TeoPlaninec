04038ae14f169e6b76cd69b43f9bd9385da4bf6e
Teo Planinec <20093089@mail.wit.ie>
2022-04-20 20:39:02 +0100

    Add officer and list officer methods

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
+import controllers.OfficerAPI
+import models.Officer
 import utils.ScannerInput
+import utils.ScannerInput.readNextDouble
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
+
+
+private val officerAPI = OfficerAPI()
 
 fun main(args: Array<String>) {
     runMenu()
@@ -33,7 +41,7 @@ fun runMenu() {
         val option = mainMenu()
         when(option) {
             1 -> addOfficer()
-            2 -> listOficer()
+            2 -> listOfficer()
             3 -> println("custom method")
             4 -> updateOfficer()
             5 -> deleteOfficer()
@@ -45,5 +53,28 @@ fun runMenu() {
             else -> println("Invalid option enteredL $option")
         }
     } while(true)
+}
+
+fun addOfficer() {
+    var name = readNextLine("Enter name of Officer: ")
+    var dateOfBirth = readNextLine("Enter date of birth of Officer: ")
+    var type = readNextLine("Enter Officer type: ")
+    var crimesSolved = readNextInt("Enter crimes solved: ")
+    var crimesUnsolved = readNextInt("Enter Crimes unsolved: ")
+    var salary = readNextDouble("Enter salary: ")
+    var peopleSaved = readNextInt("Enter people saved: ")
+    val isAdded = officerAPI.add(Officer(name, dateOfBirth, type, crimesSolved, crimesUnsolved, salary, peopleSaved))
+
+    if(isAdded) {
+        println("Added successfully")
+    } else {
+        println("Add failed")
+    }
+}
+
+fun listOfficer() {
+    var officers = officerAPI.listAllOfficers()
+
+    println(officers)
 }
 
